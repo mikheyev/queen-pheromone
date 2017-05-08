49ade12a42773380033a21d947596ec96c2e83cc
Luke Holman <luke.holman@unimelb.edu.au>
2017-05-08 17:47:20 +1000

    Wrote this script

---
+++ Script to make go_meanings table in database.R
@@ -0,0 +1,13 @@
+# Simple script to make the table 'go_meanings', which has ontology and terms for all the GOs in the bee_go table
+# Luke wrote this because loading GO.db causes many annoying conflicts with other packages
+# Restart R after running this, so that GO.db is definitely gone!
+
+library(GO.db)
+library(dplyr)
+go.meanings <- suppressMessages(AnnotationDbi::select(GO.db, unique((tbl(my_db, "bee_go") %>% dplyr::select(GO) %>% collect(n=Inf) %>% as.data.frame())[,1]), c("GOID", "ONTOLOGY", "TERM")))
+go.meanings
+names(go.meanings) <- c("GO", "ontology", "term")
+
+# Add/overwrite the new table to the database
+my_db$con %>% db_drop_table(table = "go_meanings")
+copy_to(my_db, go.meanings, "go_meanings", temporary = FALSE)
